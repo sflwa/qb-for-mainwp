cd9c28796a21b0ef038d96c8adb5941698d039af
sflwa <philip@southfloridawebadvisors.com>
2025-10-26 17:00:25 -0400

    Update and rename class-mainwp-development-admin.php to class-mainwp-quickbooks-admin.php

--- class/class-mainwp-quickbooks-admin.php
+++ class/class-mainwp-quickbooks-admin.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * MainWP Development Admin
+ * MainWP QuickBooks Admin
  *
  * This class handles the extension process.
  *
  * @package MainWP/Extensions
  */
 
- namespace MainWP\Extensions\Development;
+ namespace MainWP\Extensions\QuickBooks; // Updated namespace
 
  /**
-  * Class MainWP_Development_Admin
+  * Class MainWP_QuickBooks_Admin
   *
   * @package MainWP/Extensions
   */
-class MainWP_Development_Admin {
+class MainWP_QuickBooks_Admin { // Updated class name
 
 	public static $instance = null;
-	public $version         = '1.6';
+	public $version         = '1.0'; // New extension version
 
 	public static function get_instance() {
 		if ( null == self::$instance ) {
@@ -27,7 +27,7 @@ class MainWP_Development_Admin {
 	}
 
 	/**
-	 * MainWP_Development_Admin constructor.
+	 * MainWP_QuickBooks_Admin constructor.
 	 *
 	 * @return void
 	 */
@@ -38,10 +38,11 @@ class MainWP_Development_Admin {
 		add_action( 'mainwp_delete_site', array( &$this, 'hook_delete_site' ), 10, 1 );
 		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts' ) );
 
-		MainWP_Development_DB::get_instance()->install();
-		MainWP_Development_Ajax::get_instance();
-		MainWP_Development_Overview::get_instance();
-		MainWP_Development_Individual::get_instance();
+		// Instantiate all required components using the new class names
+		MainWP_QuickBooks_DB::get_instance()->install();
+		MainWP_QuickBooks_Ajax::get_instance();
+		MainWP_QuickBooks_Overview::get_instance();
+		MainWP_QuickBooks_Individual::get_instance();
 	}
 
 	/**
@@ -50,7 +51,7 @@ class MainWP_Development_Admin {
 	 * @return void
 	 */
 	public function localization() {
-		load_plugin_textdomain( 'mainwp-development-extension', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		load_plugin_textdomain( 'mainwp-quickbooks-extension', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); // Updated text domain
 	}
 
 	/**
@@ -62,7 +63,7 @@ class MainWP_Development_Admin {
 	 * @return mixed Array of plugin meta data.
 	 */
 	public function plugin_row_meta( $plugin_meta, $plugin_file ) {
-		if ( 'mainwp-development-extension/mainwp-development-extension.php' != $plugin_file ) {
+		if ( 'mainwp-quickbooks-extension/mainwp-quickbooks-extension.php' != $plugin_file ) {
 			return $plugin_meta;
 		}
 		$slug     = basename( $plugin_file, '.php' );
@@ -80,10 +81,22 @@ class MainWP_Development_Admin {
 	 * @return void
 	 */
 	public function admin_enqueue_scripts() {
-		if ( isset( $_GET['page'] ) && ( 'Extensions-Mainwp-Development-Extension' === $_GET['page'] || 'ManageSitesDevelopmentIndividual' === $_GET['page'] || ( 'managesites' === $_GET['page'] && isset( $_GET['dashboard'] ) ) ) ) {
-			wp_enqueue_style( 'mainwp-development-extension', MAINWP_DEVELOPMENT_PLUGIN_URL . 'css/mainwp-development-extension.css', array(), $this->version );
-			wp_enqueue_script( 'mainwp-development-extension', MAINWP_DEVELOPMENT_PLUGIN_URL . 'js/mainwp-development-extension.js', array(), $this->version, true );
+		if ( isset( $_GET['page'] ) && ( 'Extensions-Mainwp-QuickBooks-Extension' === $_GET['page'] || 'ManageSitesQuickBooksIndividual' === $_GET['page'] || ( 'managesites' === $_GET['page'] && isset( $_GET['dashboard'] ) ) ) ) {
+			wp_enqueue_style( 'mainwp-quickbooks-extension', MAINWP_QUICKBOOKS_PLUGIN_URL . 'css/mainwp-quickbooks-extension.css', array(), $this->version );
+			wp_enqueue_script( 'mainwp-quickbooks-extension', MAINWP_QUICKBOOKS_PLUGIN_URL . 'js/mainwp-quickbooks-extension.js', array(), $this->version, true );
 		}
+	}
+
+	/**
+	 * Handle MainWP site deletion hook.
+	 *
+	 * @param object $website The deleted child site object.
+	 *
+	 * @return void
+	 */
+	public function hook_delete_site( $website ) {
+        // In a real implementation, this is where you would clean up any site-specific data
+        // from the mainwp_quickbooks_mapping table.
 	}
 
 	/**
@@ -94,7 +107,7 @@ class MainWP_Development_Admin {
 	 * @return array $input Input.
 	 */
 	public function widgets_screen_options( $input ) {
-		$input['advanced-development-widget'] = __( 'Development Widget', 'mainwp-development-extension' );
+		$input['advanced-quickbooks-widget'] = __( 'QuickBooks Widget', 'mainwp-quickbooks-extension' ); // Updated text domain
 		return $input;
 	}
 }
